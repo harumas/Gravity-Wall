f90cb4a3f4c818430895f2d00a24feb5752e866c
MettoNao <nt7.00.harry@gmail.com>
2024-08-03 10:01:09 +0900

    PressurePlateタグ対応

--- GravityWall/Assets/Scripts/Module/Gimmick/PressurePlate.cs
+++ GravityWall/Assets/Scripts/Module/Gimmick/PressurePlate.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Module.Gimmick;
 using UnityEngine;
 
 public class PressurePlate : AbstractSwitch
 {
     [SerializeField] private List<AbstractGimmickAffected> gimmickAffecteds = new List<AbstractGimmickAffected>();
+    [SerializeField, Tag] private string[] targetTags;
     public override bool isOn { get => _isOn; protected set => _isOn = value; }
     private bool _isOn;
 
@@ -22,11 +24,13 @@ public class PressurePlate : AbstractSwitch
         {
             gimmick.Affect(this);
         }
+
+        Debug.Log("OnSwitch");
     }
 
     private void OnTriggerEnter(Collider collider)
     {
-        if (collider.CompareTag("Player") && !isOn)
+        if (targetTags.Any(tag => collider.CompareTag(tag)) && !isOn)
         {
             OnSwitch(true);
         }
