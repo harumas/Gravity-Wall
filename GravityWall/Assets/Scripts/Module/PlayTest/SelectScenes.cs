2be99322ecd964be7b32e16f8bbd5e608aa501b6
mocha_show <fuka.sho0224@gmail.com>
2024-08-27 11:35:22 +0900

    仮のステージ選択シーンを追加

--- GravityWall/Assets/Scripts/Module/PlayTest/SelectScenes.cs
+++ GravityWall/Assets/Scripts/Module/PlayTest/SelectScenes.cs
@@ -3,15 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 namespace Module.PlayTest
 {
     public class SelectScenes : MonoBehaviour
     {
         [SerializeField] private GameObject selectStageButton;
-        private string scenesPath = "";
+        [SerializeField] private float ButtonInterval;
+        [SerializeField] private GameObject ButtonParent;
+        private string scenesPath = @"Assets/Scenes/Level";
         private List<string> sceneNames = new List<string>();
-
+        private int index = 0;
         void Start()
         {
             // 指定したフォルダの全てのファイルを取得してListに追加
@@ -20,7 +24,8 @@ namespace Module.PlayTest
                 string[] files = Directory.GetFiles(scenesPath);
                 foreach (string file in files)
                 {
-                    sceneNames.Add(Path.GetFileName(file));
+                    if (Path.GetExtension(file) == ".unity" && Path.GetFileNameWithoutExtension(file) != "")
+                        sceneNames.Add(Path.GetFileNameWithoutExtension(file));
                 }
             }
             else
@@ -30,13 +35,39 @@ namespace Module.PlayTest
 
             for (int i = 0; i < sceneNames.Count; i++)
             {
-
+                Debug.Log(sceneNames[i]);
+                GameObject button = Instantiate(selectStageButton, ButtonParent.transform);
+                button.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = sceneNames[i];
+                button.transform.localPosition = new Vector3(0, -i * ButtonInterval, 0);
             }
         }
 
-        public void LoadScene()
+        public void UpSelect()
         {
+            if (index <= 0)
+                return;
 
+            index--;
+
+            ButtonParent.transform.localPosition = new Vector3(0, index * ButtonInterval, 0);
+        }
+
+        public void DownSelect()
+        {
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                if (index >= sceneNames.Count - 1)
+                    return;
+
+                index++;
+
+                ButtonParent.transform.localPosition = new Vector3(0, index * ButtonInterval, 0);
+            }
+        }
+     
+        public void Decision()
+        {
+            SceneManager.LoadScene(sceneNames[index]);
         }
     }
 }
