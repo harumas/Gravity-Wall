9eea00f5b172f33ef1a0f1582d1f426887eef5f3
harumas <haruto09.game@gmail.com>
2024-08-31 22:40:25 +0900

    プレイテスト用のステージセレクト機能の実装

--- GravityWall/Assets/Scripts/Module/PlayTest/SelectScenes.cs
+++ GravityWall/Assets/Scripts/Module/PlayTest/SelectScenes.cs
@@ -1,23 +1,40 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace Module.PlayTest
 {
     public class SelectScenes : MonoBehaviour
     {
         [SerializeField] private GameObject selectStageButton;
-        [SerializeField] private float ButtonInterval;
-        [SerializeField] private GameObject ButtonParent;
+        [SerializeField] private GameObject buttonParent;
         private string scenesPath = @"Assets/Scenes/Level";
-        private List<string> sceneNames = new List<string>();
-        private int index = 0;
+
         void Start()
         {
+            List<string> sceneNames = GetSceneNames();
+
+            // シーン名を元にボタンを生成
+            foreach (var stageName in sceneNames)
+            {
+                GameObject button = Instantiate(selectStageButton, buttonParent.transform);
+                button.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = stageName;
+
+                var name1 = stageName;
+                button.GetComponent<Button>().onClick.AddListener(() => { SceneManager.LoadScene(name1); });
+            }
+
+            EventSystem.current.firstSelectedGameObject = buttonParent.transform.GetChild(0).gameObject;
+        }
+
+        private List<string> GetSceneNames()
+        {
+            List<string> sceneNames = new List<string>();
+
             // 指定したフォルダの全てのファイルを取得してListに追加
             if (Directory.Exists(scenesPath))
             {
@@ -33,41 +50,7 @@ namespace Module.PlayTest
                 Debug.LogError("フォルダが存在しません: " + scenesPath);
             }
 
-            for (int i = 0; i < sceneNames.Count; i++)
-            {
-                Debug.Log(sceneNames[i]);
-                GameObject button = Instantiate(selectStageButton, ButtonParent.transform);
-                button.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = sceneNames[i];
-                button.transform.localPosition = new Vector3(0, -i * ButtonInterval, 0);
-            }
-        }
-
-        public void UpSelect()
-        {
-            if (index <= 0)
-                return;
-
-            index--;
-
-            ButtonParent.transform.localPosition = new Vector3(0, index * ButtonInterval, 0);
-        }
-
-        public void DownSelect()
-        {
-            if (Input.GetKeyDown(KeyCode.DownArrow))
-            {
-                if (index >= sceneNames.Count - 1)
-                    return;
-
-                index++;
-
-                ButtonParent.transform.localPosition = new Vector3(0, index * ButtonInterval, 0);
-            }
-        }
-     
-        public void Decision()
-        {
-            SceneManager.LoadScene(sceneNames[index]);
+            return sceneNames;
         }
     }
 }
