715ed22cd84f6523976b4b4f5dc6a7806323849e
harumas <haruto09.game@gmail.com>
2024-09-24 14:15:05 +0900

    コリジョンマトリックスの修正

--- GravityWall/Assets/Scripts/CoreModule/Constants/Layer.cs
+++ GravityWall/Assets/Scripts/CoreModule/Constants/Layer.cs
@@ -10,24 +10,26 @@ namespace Constants
 		public const int Default = 0;
 		public const int TransparentFX = 1;
 		public const int IgnoreRaycast = 2;
+		public const int Player = 3;
 		public const int Water = 4;
 		public const int UI = 5;
 		public const int Gravity = 6;
 		public const int Base = 7;
-		public const int Holdable = 8;
 		public const int IgnoreGravity = 9;
+		public const int Volume = 10;
 		
 		public sealed class Mask
 		{
 			public const int Default = 1;
 			public const int TransparentFX = 2;
 			public const int IgnoreRaycast = 4;
+			public const int Player = 8;
 			public const int Water = 16;
 			public const int UI = 32;
 			public const int Gravity = 64;
 			public const int Base = 128;
-			public const int Holdable = 256;
 			public const int IgnoreGravity = 512;
+			public const int Volume = 1024;
 		}
 	}
 }
