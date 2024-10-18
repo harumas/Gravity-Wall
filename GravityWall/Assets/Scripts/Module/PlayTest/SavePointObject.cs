0352557cb05956311863d85dc5d02ed7ddcf0918
Mocha0924 <fuka.sho0224@gmail.com>
2024-10-18 14:23:07 +0900

    セーブポイントごとに重力を変更できるように修正

--- GravityWall/Assets/Scripts/Module/PlayTest/SavePointObject.cs
+++ GravityWall/Assets/Scripts/Module/PlayTest/SavePointObject.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 using Module.Gimmick;
 using Module.Gravity;
 using Constants;
+using Cinemachine;
 namespace Module.PlayTest
+
 {
     public class SavePointObject : MonoBehaviour
     {
+        private enum Gravity
+        {
+            down,
+            left,
+            up,
+            right,
+            forward,
+            back
+        }
+        [SerializeField] private Gravity gravity;
         private RespawnManager respawnManager => RespawnManager.instance;
         private bool FirstTouch = false;
         [SerializeField] private GameObject RetryPositionObject;
@@ -18,10 +30,24 @@ namespace Module.PlayTest
             {
                 Debug.Log("セーブしました");
                 respawnManager.RetryPosition = RetryPositionObject.transform.position;
-                respawnManager.GravityScale = WorldGravity.Instance.Gravity;
+                respawnManager.GravityScale = ChangeGravity();
                 FirstTouch = true;
                 respawnManager.ObjectReset();
             }
+        }
+
+        private Vector3 ChangeGravity()
+        {
+            switch(gravity)
+            { 
+                case Gravity.down:return Vector3.down;
+                case Gravity.left:return Vector3.left;
+                case Gravity.right:return Vector3.right;
+                case Gravity.up:return Vector3.up;
+                case Gravity.forward:return Vector3.forward;
+                case Gravity.back:return Vector3.back;
+            }
+            return Vector3.down;
         }
     }
 }
