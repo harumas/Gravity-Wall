ce272b89d987028bc353e9bb1c890bf475ccd3d4
harumas <haruto09.game@gmail.com>
2024-11-06 21:33:21 +0900

    wip

--- GravityWall/Assets/Scripts/Module/Gimmick/GimmickReference.cs
+++ GravityWall/Assets/Scripts/Module/Gimmick/GimmickReference.cs
@@ -5,13 +5,31 @@ namespace Module.Gimmick
 {
     public class GimmickReference : MonoBehaviour
     {
-        private readonly Dictionary<string, GimmickObject> gimmickObjects = new Dictionary<string, GimmickObject>();
+        private static readonly Dictionary<string, GimmickObject> gimmickObjects = new Dictionary<string, GimmickObject>();
 
         public void UpdateReference()
         {
-            
+            GimmickObject[] gimmicks = FindObjectsByType<GimmickObject>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+            gimmickObjects.Clear();
+
+            foreach (GimmickObject gimmick in gimmicks)
+            {
+                gimmickObjects.Add(gimmick.Path, gimmick);
+            }
         }
 
-        public static bool TryGetGimmick<T>() where T : GimmickObject { }
+        public static bool TryGetGimmick<T>(string gimmickPath, out T gimmick) where T : GimmickObject
+        {
+            if (gimmickObjects.TryGetValue(gimmickPath, out GimmickObject gimmickObject) &&
+                gimmickObject is T item)
+            {
+                gimmick = item;
+                return true;
+            }
+
+            gimmick = null;
+            return false;
+        }
+        
     }
 }
--- GravityWall/Assets/Scripts/Module/Gimmick/Hallway.cs
+++ GravityWall/Assets/Scripts/Module/Gimmick/Hallway.cs
@@ -1,20 +1,68 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Constants;
+using R3;
+using TriInspector;
 using UnityEngine;
 
 namespace Module.Gimmick
 {
     public class Hallway : MonoBehaviour
     {
+        [SerializeField] private GameObject hallwayBody;
         [SerializeField] private List<string> gimmickObjects;
+        [SerializeField, ReadOnly] private List<Gate> referencedGates;
+
+        private bool isPlayerEnter;
+        private Gate enteredGate;
 
         private void Awake()
         {
             foreach (string path in gimmickObjects)
             {
-                string[] names = path.Split('/');
-                
+                if (GimmickReference.TryGetGimmick(path, out Gate gate))
+                {
+                    referencedGates.Add(gate);
+                    gate.IsEnabled.Subscribe(isEnabled => OnGateAction(gate, isEnabled)).AddTo(this);
+                }
+            }
+        }
+
+        private void OnGateAction(Gate gate, bool isEnabled)
+        {
+            if (isEnabled)
+            {
+                hallwayBody.SetActive(true);
+                enteredGate = gate;
+            }
+            else if (!isPlayerEnter)
+            {
+                hallwayBody.SetActive(false);
+
+                foreach (Gate otherGate in referencedGates)
+                {
+                    if (gate != otherGate)
+                    {
+                        otherGate.gameObject.SetActive(false);
+                    }
+                }
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag(Tag.Player))
+            {
+                isPlayerEnter = true;
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag(Tag.Player))
+            {
+                isPlayerEnter = false;
             }
         }
     }
