e1047fa861aa76aeaf8cbc4b7700aca71aabb517
harumas <haruto09.game@gmail.com>
2024-11-21 21:34:21 +0900

    コメントの追加

--- GravityWall/Assets/Scripts/Module/Gimmick/LevelGimmick/EmissionByDistance.cs
+++ GravityWall/Assets/Scripts/Module/Gimmick/LevelGimmick/EmissionByDistance.cs
@@ -1,17 +1,16 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using CoreModule.Helper;
 using UnityEngine;
 
-
-namespace Gimmick.LevelGimmick
+namespace Module.Gimmick.LevelGimmick
 {
+    /// <summary>
+    /// カメラとの距離によってエミッションの強さを変えるクラス
+    /// </summary>
     public class EmissionByDistance : MonoBehaviour
     {
         [SerializeField] private Renderer targetRenderer;
-        [SerializeField] private MinMaxValue distanceRange;
-        [SerializeField] private float minEmission;
+        [SerializeField, Header("エミッションを変化させる範囲")] private MinMaxValue distanceRange;
+        [SerializeField, Header("最低エミッション")] private float minEmission;
 
         private Camera mainCamera;
         private static readonly int emissionIntensityProperty = Shader.PropertyToID("_EmissionIntensity");
@@ -25,8 +24,7 @@ namespace Gimmick.LevelGimmick
         {
             Vector3 p = transform.position - mainCamera.transform.position;
 
-            Debug.Log(distanceRange.Remap01Squared(p.x * p.x + p.y * p.y + p.z * p.z));
-
+            // カメラとの距離を0 ~ 1にリマップしてエミッションの強さに変換する
             float intensity = Mathf.Max(minEmission, distanceRange.Remap01Squared(p.x * p.x + p.y * p.y + p.z * p.z));
 
             targetRenderer.material.SetFloat(emissionIntensityProperty, intensity);
