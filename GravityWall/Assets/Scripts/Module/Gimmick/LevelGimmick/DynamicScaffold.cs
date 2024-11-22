73f850336236d4290aeba9b8518abbe715c23d1a
Mocha0924 <fuka.sho0224@gmail.com>
2024-11-22 19:09:47 +0900

    move-floor-easyのエレベータをボタンが押された時に移動するように修正

--- GravityWall/Assets/Scripts/Module/Gimmick/LevelGimmick/DynamicScaffold.cs
+++ GravityWall/Assets/Scripts/Module/Gimmick/LevelGimmick/DynamicScaffold.cs
@@ -4,6 +4,7 @@ using Constants;
 using Cysharp.Threading.Tasks;
 using Cysharp.Threading.Tasks.Linq;
 using Domain;
+using R3;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -22,6 +23,9 @@ namespace Module.Gimmick.DynamicScaffold
 
         [Header("最初から動かすか")] [SerializeField] private bool enableOnAwake = true;
 
+        [SerializeField] private GimmickObject[] observedSwitches;
+        [SerializeField] private int switchMaxCount = 1;
+        private int switchCount = 0;
         private CancellationTokenSource cTokenSource;
         private Rigidbody rigBody;
         private Vector3 previousTargetPosition;
@@ -57,11 +61,30 @@ namespace Module.Gimmick.DynamicScaffold
 
         private void Start()
         {
+            foreach (GimmickObject gimmick in observedSwitches)
+            {
+                gimmick.IsEnabled.Skip(1).Subscribe(UpdateMoveState).AddTo(this);
+            }
             if (enableOnAwake)
             {
                 Enable();
             }
         }
+        private void UpdateMoveState(bool switchEnabled)
+        {
+            switchCount += switchEnabled ? 1 : -1;
+            bool isMove = switchCount >= switchMaxCount;
+
+            if (isMove)
+            {
+                Enable();
+            }
+            else
+            {
+                Disable();
+            }
+        }
+
 
         public override void Enable(bool doEffect = true)
         {
