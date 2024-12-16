32f933fd1d3328bfbc1cd708432e07c3c216489c
harumas <haruto09.game@gmail.com>
2024-12-16 19:41:39 +0900

    wip

--- GravityWall/Assets/Scripts/Module/Gimmick/SystemGimmick/ObjectDitherObserver.cs
+++ GravityWall/Assets/Scripts/Module/Gimmick/SystemGimmick/ObjectDitherObserver.cs
@@ -1,27 +1,31 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Constants;
 using UnityEngine;
 
 namespace Module.Gimmick.SystemGimmick
 {
+    /// <summary>
+    /// カメラとプレイヤーの間のオブジェクトを透過するクラス
+    /// </summary>
     public class ObjectDitherObserver : MonoBehaviour
     {
         [SerializeField] private Transform playerTransform;
         [SerializeField] private float maxDistance;
-        private readonly RaycastHit[] hitResults = new RaycastHit[16];
-        private readonly Dictionary<GameObject, ObjectDither> hitObjects = new Dictionary<GameObject, ObjectDither>(16);
-        private readonly HashSet<GameObject> containedObjects = new HashSet<GameObject>(16);
-        private readonly List<GameObject> removeObjectsCache = new List<GameObject>(16);
+
+        private const int maxBufferCount = 16;
+        private readonly RaycastHit[] hitResults = new RaycastHit[maxBufferCount];
+        private readonly Dictionary<GameObject, ObjectDither> hitObjects = new(maxBufferCount);
+        private readonly HashSet<GameObject> containedObjects = new(maxBufferCount);
+        private readonly List<GameObject> removeObjectsCache = new(maxBufferCount);
         private const int layerMask = Layer.Mask.Base | Layer.Mask.IgnoreGravity;
 
         private void Update()
         {
+            // カメラからプレイヤーまでのオブジェクトを取得
             Ray ray = new Ray(transform.position, playerTransform.position - transform.position);
-
             int hitCount = Physics.RaycastNonAlloc(ray, hitResults, maxDistance, layerMask, QueryTriggerInteraction.Collide);
 
+            // 
             for (int i = 0; i < hitCount; i++)
             {
                 GameObject hitObj = hitResults[i].transform.gameObject;
@@ -33,17 +37,14 @@ namespace Module.Gimmick.SystemGimmick
 
                 containedObjects.Add(hitObj);
 
-                if (!hitObjects.ContainsKey(hitObj))
+                if (hitObjects.Count == hitResults.Length || hitObjects.ContainsKey(hitObj))
                 {
-                    if (hitObjects.Count == hitResults.Length)
-                    {
-                        continue;
-                    }
-
-                    var dither = hitObj.GetComponent<ObjectDither>();
-                    hitObjects.Add(hitObj, dither);
-                    dither.Show();
+                    continue;
                 }
+
+                var dither = hitObj.GetComponent<ObjectDither>();
+                hitObjects.Add(hitObj, dither);
+                dither.Show();
             }
 
             foreach ((GameObject obj, ObjectDither dither) in hitObjects)
