65f898a0b7c3fff02329504988d0e3587667c9b1
harumas <haruto09.game@gmail.com>
2025-01-06 17:36:43 +0900

    wip

---
+++ GravityWall/Assets/Scripts/Presentation/HubEventPresenter.cs
@@ -0,0 +1,34 @@
+﻿using Application.Spawn;
+using Cysharp.Threading.Tasks;
+using Module.Gimmick.LevelGimmick;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+namespace Presentation
+{
+    public class HubEventPresenter : IStartable
+    {
+        private readonly HubSpawner hubSpawner;
+
+        [Inject]
+        public HubEventPresenter(HubSpawner hubSpawner)
+        {
+            this.hubSpawner = hubSpawner;
+        }
+
+        public void Start()
+        {
+            var deathFloors = Object.FindObjectsByType<DeathFloor>(FindObjectsSortMode.None);
+
+            //死亡床のイベント登録
+            foreach (DeathFloor deathFloor in deathFloors)
+            {
+                deathFloor.OnEnter += (type) =>
+                {
+                    hubSpawner.Respawn().Forget();
+                };
+            }
+        }
+    }
+}
