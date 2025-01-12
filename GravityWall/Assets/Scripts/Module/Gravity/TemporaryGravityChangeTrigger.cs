6cd98b6ce2c1c0406ebbfe57b1218a73999de6f8
harumas <haruto09.game@gmail.com>
2025-01-12 14:56:57 +0900

    扉に落下したときにカメラが追えない問題の修正

---
+++ GravityWall/Assets/Scripts/Module/Gravity/TemporaryGravityChangeTrigger.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Constants;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Module.Gravity
+{
+    public class TemporaryGravityChangeTrigger : MonoBehaviour
+    {
+        [SerializeField] private float temporaryGravityMultiplier;
+        [SerializeField] private Vector3 constraintDirection;
+        [SerializeField] private float velocityMultiplier;
+
+        private bool isPlayerEnter;
+        private LocalGravity targetGravity;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag(Tag.Player) && IsConstrainedDirection(other.transform.up))
+            {
+                targetGravity = other.gameObject.GetComponent<LocalGravity>();
+                Rigidbody rig = other.gameObject.GetComponent<Rigidbody>();
+                rig.velocity *= velocityMultiplier;
+                isPlayerEnter = true;
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.CompareTag(Tag.Player))
+            {
+                isPlayerEnter = false;
+            }
+        }
+        
+        private bool IsConstrainedDirection(Vector3 direction)
+        {
+            return Vector3.Dot(constraintDirection, direction) > 0.5f;
+        }
+
+        private void FixedUpdate()
+        {
+            if (isPlayerEnter)
+            {
+                targetGravity.SetMultiplierAtFrame(temporaryGravityMultiplier);
+            }
+        }
+    }
+}
